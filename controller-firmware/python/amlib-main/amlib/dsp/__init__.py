13cd4f360e0d8a0de53452f5ddb056409dac66d3
Excessive Overkill <46392388+ExcessiveOverkill@users.noreply.github.com>
2024-09-04 12:42:13 -0400

    Delete controller-firmware/python/amlib-main directory

--- controller-firmware/python/amlib-main/amlib/dsp/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resampler               import FractionalResampler
-from .fixedpointiirfilter     import FixedPointIIRFilter
-from .fixedpointfirfilter     import FixedPointFIRFilter
-from .filterbank              import Filterbank
-from .fixedpointcicfilter     import FixedPointCICFilter
-from .fixedpointhbfilter      import FixedPointHBFilter
-from .fixedpointfft           import FixedPointFFT
-
-__all__ = [FractionalResampler, FixedPointIIRFilter, FixedPointFIRFilter, Filterbank, FixedPointCICFilter, FixedPointHBFilter, FixedPointFFT]
--- controller-firmware/python/amlib-main/amlib/dsp/convolution/__init__.py
+++
@@ -1,4 +0,0 @@
-from .convolutionmode 	import ConvolutionMode
-from .mac  		import StereoConvolutionMAC
-
-__all__ = [StereoConvolutionMAC, ConvolutionMode]
