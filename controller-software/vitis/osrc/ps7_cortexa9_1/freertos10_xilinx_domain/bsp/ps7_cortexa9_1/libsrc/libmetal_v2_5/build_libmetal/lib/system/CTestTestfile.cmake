fcc0d00491bc58c05c6981ba35bd36e2bc0555d5
Excessive Overkill <46392388+ExcessiveOverkill@users.noreply.github.com>
2025-01-14 20:35:00 -0500

    move and remove folders

--- controller-software/vitis/osrc/ps7_cortexa9_1/freertos10_xilinx_domain/bsp/ps7_cortexa9_1/libsrc/libmetal_v2_5/build_libmetal/lib/system/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/excessive/github/controller-software/controller-software/vitis/osrc/ps7_cortexa9_1/freertos10_xilinx_domain/bsp/ps7_cortexa9_1/libsrc/libmetal_v2_5/src/libmetal/lib/system
-# Build directory: /home/excessive/github/controller-software/controller-software/vitis/osrc/ps7_cortexa9_1/freertos10_xilinx_domain/bsp/ps7_cortexa9_1/libsrc/libmetal_v2_5/build_libmetal/lib/system
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(freertos)
